a6aa96128ce3bd96cfdfd8830b5e43b415ab9eeb
aditia7962 <aditia7962@students.amikom.ac.id>
2016-11-18 22:34:29 +0700

    this is first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php  
+
+require ('lib/hallo.class.php');
+
+$string = "Adit";
+
+$hallo = new Hallo;
+echo $hallo->sapa($string);
+
+?>
---
+++ lib/hallo.class.php
@@ -0,0 +1,11 @@
+<?php  
+
+	class Hallo{
+		var $string;
+
+		function sapa($string){
+			return $this->string = "Hello World ".$string;
+		}
+	}
+
+?>
